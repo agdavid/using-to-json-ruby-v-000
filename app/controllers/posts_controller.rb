7058f1241ffc240b50efca9481175780158503db
agdavid <antonio.g.david@gmail.com>
2016-06-18 11:19:03 -0400

    include only limited info in API endpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,7 +32,8 @@ class PostsController < ApplicationController
     #render json: PostSerializer.serialize(post)
 
     #this is how we use rails' built in serializer to JSON
-    render json: post.to_json(include: :author)
+    render json: post.to_json(only: [:title, :description, :id],
+                              include: [ author: { only: [:name]}])
   end
 
 private
